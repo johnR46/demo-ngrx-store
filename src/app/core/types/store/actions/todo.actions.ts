72877997afe26580f5ebc3ca8e462d905a2296b4
John nonphala <johnnote7@gmail.com>
2020-01-13 22:28:25 +0700

    updateReducer

--- src/app/core/types/store/actions/todo.actions.ts
+++ src/app/core/types/store/actions/todo.actions.ts
@@ -1,39 +1,43 @@
 import { createAction, props } from '@ngrx/store';
 import { MODE } from '../constant/mode';
+import { MenuId } from '../constant/menu-id';
 
 export const CreateTodo = createAction(
   '[Todo] CreateTodo',
-  props<{ featKey: string }>()
+  props<{ featKey: MenuId }>()
 );
 
 export const CreateTodoSuccess = createAction(
   '[Todo] CreateTodoSuccess',
-  props<{ featKey: string; formValue: {} }>()
+  props<{ featKey: MenuId; formValue: {} }>()
 );
 
 export const SearchTodoSuccess = createAction(
   '[Todo] SearchTodoSuccess',
-  props<{ featKey: string; criteria: {}; result: [] }>()
+  props<{ featKey: MenuId; criteria: {}; result: Array<Object> }>()
 );
 
 export const SearchTodoFailed = createAction(
   '[Todo] SearchTodoFailed',
-  props<{ featKey: string; criteria?: {} }>()
+  props<{ featKey: MenuId; criteria?: {} }>()
 );
 
-export const ResetTodo = createAction('[Todo ResetTodo]');
+export const ResetTodo = createAction(
+  '[Todo ResetTodo]',
+  props<{ featKey: MenuId }>()
+);
 
 export const ViewTodo = createAction(
   '[Todo] ViewTodo',
-  props<{ featKey: string; formValue: {} }>()
+  props<{ featKey: MenuId; formValue: {} }>()
 );
 
 export const UpdateTodo = createAction(
   '[Todo] UpdateTodo ',
-  props<{ featKey: string; activeIndex: number; formValue: {} }>()
+  props<{ featKey: MenuId; activeIndex: number; formValue: {} }>()
 );
 
 export const UpdateTodoSuccess = createAction(
   '[Todo] UpdateTodoSuccess ',
-  props<{ featKey: string; formValue: {} }>()
+  props<{ featKey: MenuId; formValue: {} }>()
 );
