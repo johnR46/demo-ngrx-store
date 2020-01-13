1214a802e3b24848473a49bdc417fffccca1bc66
John nonphala <johnnote7@gmail.com>
2020-01-13 22:28:52 +0700

    fix FeatStore

--- src/app/core/types/store/model/feat-store.ts
+++ src/app/core/types/store/model/feat-store.ts
@@ -2,30 +2,38 @@ import { MODE } from '../constant/mode';
 
 export class FeatStore {
   constructor(
+    private criteria: Object,
     private formValue: Object,
     private activeIndex: number,
     private mode: MODE,
     private result: Array<Object>
-  ) {
-    this.formValue = formValue;
-    this.activeIndex = activeIndex;
+  ) {}
+
+  setCriteria(criteria: Object): void {
+    this.criteria = criteria;
+  }
+
+  setFormValue(form: Object): void {
+    this.formValue = form;
+  }
+
+  getCriteria(): Object {
+    return this.criteria;
+  }
+
+  getFormValue(): Object {
+    return this.formValue;
+  }
+
+  setActiveIndex(index: number): void {
+    this.activeIndex = index;
+  }
+  setMode(mode: MODE): void {
     this.mode = mode;
-    this.result = result;
   }
 
   setResult(result: Array<Object>) {
     this.result = result;
-  }
-
-  setActiveIndex(activeIndex: number) {
-    this.activeIndex = activeIndex;
-  }
-  setFormValue(formValue: Object) {
-    this.formValue = formValue;
-  }
-
-  setMode(mode: MODE) {
-    this.mode = mode;
   }
 
   getResult(): Array<Object> {
@@ -36,9 +44,5 @@ export class FeatStore {
   }
   getActiveIndex(): number {
     return this.activeIndex;
-  }
-
-  getFormValue(): Object {
-    return this.formValue;
   }
 }
